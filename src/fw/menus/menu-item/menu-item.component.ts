56bf01e0cb842d33d23503871328d9763b096eb2
frenzy777 <stefantrajkovic777@gmail.com>
2017-04-03 15:48:51 +0200

    show active routes in popup

--- src/fw/menus/menu-item/menu-item.component.ts
+++ src/fw/menus/menu-item/menu-item.component.ts
@@ -1,6 +1,8 @@
-import { Component, ElementRef, HostBinding, HostListener,
-         Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  Component, ElementRef, HostBinding, HostListener,
+  Input, OnInit, Renderer
+} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
 
 import { MenuItem, MenuService } from '../../services/menu.service';
 
@@ -20,11 +22,46 @@ export class MenuItemComponent implements OnInit {
   popupLeft = 0;
   popupTop = 34;
 
-  constructor(private router:Router, 
-              private menuService: MenuService) {
+  constructor(private router:Router,
+              private menuService: MenuService,
+              private el : ElementRef,
+              private renderer : Renderer) {
+  }
+
+  checkActiveRoute(route : string) {
+    this.isActiveRoute = (route == '/' + this.item.route);
   }
 
   ngOnInit() : void {
+    this.checkActiveRoute(this.router.url);
+
+    this.router.events
+      .subscribe((event) => {
+          if(event instanceof NavigationEnd) {
+            this.checkActiveRoute(event.url);
+            console.log(event.url + ' ' + this.item.route + ' ' + this.isActiveRoute)
+          }
+      });
+  }
+
+  @HostListener('click', ['$event'])
+  onClick(event) : void {
+
+    event.stopPropagation();
+
+    if (this.item.submenu) {
+      if (this.menuService.isVertical) {
+        this.mouseInPopup = !this.mouseInPopup;
+      }
+    }
+    else if (this.item.route) {
+      // force horizontal menus to close by sending a mouseleave event
+      let newEvent = new MouseEvent('mouseleave', {bubbles: true});
+      this.renderer.invokeElementMethod(
+        this.el.nativeElement, 'dispatchEvent', [newEvent]);
+
+      this.router.navigate(['/' + this.item.route]);
+    }
   }
 
   onPopupMouseEnter(event) : void {
@@ -46,7 +83,7 @@ export class MenuItemComponent implements OnInit {
       }
   }
 
-  @HostListener('mouseenter') 
+  @HostListener('mouseenter')
   onMouseEnter() : void {
       if (!this.menuService.isVertical) {
           if (this.item.submenu) {
