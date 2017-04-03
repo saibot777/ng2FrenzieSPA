7a3fa0c4a90818a7e54bf044c5efc8c7a87b08fa
frenzy777 <stefantrajkovic777@gmail.com>
2017-04-03 14:54:10 +0200

    popup-menu created with event listeners

--- src/fw/menus/menu-item/menu-item.component.ts
+++ src/fw/menus/menu-item/menu-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener,
+         Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { MenuItem, MenuService } from '../../services/menu.service';
 
@@ -9,10 +11,51 @@ import { MenuItem, MenuService } from '../../services/menu.service';
 })
 export class MenuItemComponent implements OnInit {
   @Input() item = <MenuItem>null;  // see angular-cli issue #2034
+  @HostBinding('class.parent-is-popup')
+  @Input() parentIsPopup = true;
+  isActiveRoute = false;
 
-  constructor(private menuService: MenuService) { }
+  mouseInItem = false;
+  mouseInPopup = false;
+  popupLeft = 0;
+  popupTop = 34;
 
-  ngOnInit() {
+  constructor(private router:Router, 
+              private menuService: MenuService) {
   }
 
+  ngOnInit() : void {
+  }
+
+  onPopupMouseEnter(event) : void {
+      if (!this.menuService.isVertical) {
+          this.mouseInPopup = true;
+      }
+  }
+
+  onPopupMouseLeave(event) : void {
+      if (!this.menuService.isVertical) {
+          this.mouseInPopup = false;
+      }
+  }
+
+  @HostListener('mouseleave', ['$event'])
+  onMouseLeave(event) : void {
+      if (!this.menuService.isVertical) {
+          this.mouseInItem = false;
+      }
+  }
+
+  @HostListener('mouseenter') 
+  onMouseEnter() : void {
+      if (!this.menuService.isVertical) {
+          if (this.item.submenu) {
+              this.mouseInItem = true;
+              if (this.parentIsPopup) {
+                  this.popupLeft = 160;
+                  this.popupTop = 0;
+              }
+          }
+      }
+  }
 }
--- src/fw/services/menu-service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import {MenuItem} from "./item.model";
-
-@Injectable()
-export class MenuService {
-
-  items: Array<MenuItem>;
-
-}
