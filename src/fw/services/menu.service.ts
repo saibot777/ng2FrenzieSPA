a92d81f4092b6edaa015963b942fd21c7ef99cb1
frenzy777 <stefantrajkovic777@gmail.com>
2017-04-06 15:16:15 +0200

    auth done

--- src/fw/services/menu.service.ts
+++ src/fw/services/menu.service.ts
@@ -15,11 +15,12 @@ export class MenuService {
   showingLeftSideMenu = false;
 
   toggleLeftSideMenu() : void {
+    this.isVertical = true;
     this.showingLeftSideMenu = !this.showingLeftSideMenu;
   }
 
   toggleMenuOrientation() {
     this.isVertical = !this.isVertical;
   }
-
+  
 }
